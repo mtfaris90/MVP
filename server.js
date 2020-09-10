a403a0db4b94ba9f3be8a19412762a197d455bab
Matthew Faris <mtfaris90@gmail.com>
2020-09-09 20:51:22 -0400

    capitalize port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 const controllers = require("./controllers");
 const bodyParser = require("body-parser");
 
